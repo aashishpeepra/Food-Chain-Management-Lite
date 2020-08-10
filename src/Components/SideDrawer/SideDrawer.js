fefde4ff12e118afdeeab784f7897748ac28c5ad
aashishpeepra <ironhack082327@gmail.com>
2020-08-10 22:56:56 +0530

    NavBar

--- src/Components/SideDrawer/SideDrawer.js
+++ src/Components/SideDrawer/SideDrawer.js
@@ -10,55 +10,37 @@ const sideDrawer = (props) => {
   }
   return (
     <nav className={classes.join(" ")}>
-      <div className="Info__Box">
-        <div className="Name__Box">
-          {!props.loggedIn ? "" : props.userInfo.name}
-        </div>
-        <div className="Email__Box">
-          {!props.loggedIn ? "" : props.userInfo.email}
-        </div>
-      </div>
+    
       <div className="Options">
         <div className="Options__Home">
           <NavLink to="/dashboard">Dashboard</NavLink>
         </div>
         <div className="Options__Home">
-          <NavLink to="/">Boys</NavLink>
+          <NavLink to="/forms/stockavailable">Stock Available</NavLink>
         </div>
         <div className="Options__Home">
-          <NavLink to="/">Girls</NavLink>
+          <NavLink to="/forms/stockreceived">Stock Received</NavLink>
         </div>
         <div className="Options__Home">
-          <NavLink to="/">Baby Boys</NavLink>
+          <NavLink to="/forms/hubtransfer">Hub Transfer</NavLink>
         </div>
         <div className="Options__Home">
-          <NavLink to="/">Baby Girls</NavLink>
+          <NavLink to="/forms/carcass">Carcass Converter</NavLink>
         </div>
         <div className="Options__Cart">
-          <NavLink to="/cart">Cart</NavLink>
+          <NavLink to="/forms/returnfromhub">Return From Hub</NavLink>
         </div>
-        {
-          props.loggedIn ? (
-            <React.Fragment>
-              <div className="Options__Orders">
-                <NavLink to="/user">My Orders</NavLink>
-              </div>
-              <div className="Options__Account">
-                <NavLink to="/user">My Account</NavLink>
-              </div>
-            </React.Fragment>
-
-          ) : null
-        }
-
-        <div className="Options__Log">
-          <NavLink
-            onClick={props.loggedIn ? logout : () => { }}
-            to={props.loggedIn ? "logout" : "login"}
-          >
-            {props.loggedIn ? "Log out" : "Log in"}
-          </NavLink>
+        
+        <div className="Options__Cart">
+          <NavLink to="/forms/deliverysla">Delivery SLA</NavLink>
         </div>
+        
+        <div className="Options__Cart">
+          <NavLink to="/forms/salesentry">Sales Entry</NavLink>
+        </div>
+      
+
+        
       </div>
     </nav>
   );
