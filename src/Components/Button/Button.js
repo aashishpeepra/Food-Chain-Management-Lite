3e5d7d4e74105352a7c6ecf882825483d502eb7f
aashishpeepra <ironhack082327@gmail.com>
2020-08-11 14:37:16 +0530

    Added Sales Entry

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -2,6 +2,6 @@ import React from "react";
 import "./Button.css";
 export default (props)=>{
     return (
-    <button type="button" onClick={props.func} className="btn">{props.value}</button>
+    <button type="button" onClick={props.func} className="btnMy">{props.value}</button>
     )
 }
